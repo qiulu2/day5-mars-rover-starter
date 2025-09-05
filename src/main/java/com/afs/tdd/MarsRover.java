fed27882a437d3a36545cbbd0a808cdc84db584f
qiulu2 <luca.qiu@oocl.com>
2025-09-05 16:07:34 +0800

    feat: refactor MarsRover command execution and add unit 12 tests for movement and turning

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -25,27 +25,70 @@ public class MarsRover {
         this.direction = direction;
     }
 
-    public void executeCommand(String command) {
-        if (command.equals("M")) {
-            switch (direction) {
-                case "N":
-                    this.y += 1;
-                    break;
-                case "S":
-                    this.y -= 1;
-                    break;
-                case "E":
-                    this.x += 1;
-                    break;
-                case "W":
-                    this.x -= 1;
-                    break;
-            }
+    public void executeCommand(String command){
+        switch (command) {
+            case "M":
+                move();
+                break;
+            case "L":
+                turnLeft();
+                break;
+            case "R":
+                turnRight();
+                break;
         }
     }
 
+    public void move() {
+        switch (direction) {
+            case "N":
+                this.y += 1;
+                break;
+            case "S":
+                this.y -= 1;
+                break;
+            case "E":
+                this.x += 1;
+                break;
+            case "W":
+                this.x -= 1;
+                break;
+        }
+    }
 
+    public void turnLeft() {
+        switch (direction) {
+            case "N":
+                this.direction = "W";
+                break;
+            case "W":
+                this.direction = "S";
+                break;
+            case "S":
+                this.direction = "E";
+                break;
+            case "E":
+                this.direction = "N";
+                break;
+        }
+    }
 
+    public void turnRight() {
+        switch (direction) {
+            case "N":
+                this.direction = "E";
+                break;
+            case "E":
+                this.direction = "S";
+                break;
+            case "S":
+                this.direction = "W";
+                break;
+            case "W":
+                this.direction = "N";
+                break;
+        }
+    }
 
 
 }
--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -4,17 +4,118 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class DemoTest {
+    // move
+    @Test
+    void should_return_X1_Y0_W_when_execute_command_M() {
+        MarsRover marsRover = new MarsRover(0, 0, "W");
+
+        marsRover.executeCommand("M");
+
+        Assertions.assertEquals(-1, marsRover.getX());
+    }
+
+    @Test
+    void should_return_X0_Y1_W_when_execute_command_M() {
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+
+        marsRover.executeCommand("M");
+
+        Assertions.assertEquals(1, marsRover.getY());
+    }
+
+    @Test
+    void should_return_X0_Y_1_W_when_execute_command_M() {
+        MarsRover marsRover = new MarsRover(0, 0, "S");
+
+        marsRover.executeCommand("M");
+
+        Assertions.assertEquals(-1, marsRover.getY());
+    }
+
     @Test
     void should_return_X1_Y0_E_when_execute_command_M() {
-        String command = "M";
+        MarsRover marsRover = new MarsRover(0, 0, "E");
 
-        MarsRover marsRover = new MarsRover(0,0,"N");
-        marsRover.executeCommand(command);
+        marsRover.executeCommand("M");
 
-        Assertions.assertEquals(-1,marsRover.getX());
+        Assertions.assertEquals(1, marsRover.getX());
+    }
+
+    // turn left
+    @Test
+    void should_return_X0_Y0_S_when_execute_command_L() {
+        MarsRover marsRover = new MarsRover(0, 0, "W");
+
+        marsRover.executeCommand("L");
+
+        Assertions.assertEquals("S", marsRover.getDirection());
+    }
+
+    @Test
+    void should_return_X0_Y0_E_when_execute_command_L() {
+        MarsRover marsRover = new MarsRover(0, 0, "S");
+
+        marsRover.executeCommand("L");
+
+        Assertions.assertEquals("E", marsRover.getDirection());
+    }
+
+    @Test
+    void should_return_X0_Y0_N_when_execute_command_L() {
+        MarsRover marsRover = new MarsRover(0, 0, "E");
+
+        marsRover.executeCommand("L");
+
+        Assertions.assertEquals("N", marsRover.getDirection());
+    }
+
+    @Test
+    void should_return_X0_Y0_W_when_execute_command_L() {
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+
+        marsRover.executeCommand("L");
+
+        Assertions.assertEquals("W", marsRover.getDirection());
     }
 
 
+    // turn right
+    @Test
+    void should_return_X0_Y0_N_when_execute_command_R() {
+        MarsRover marsRover = new MarsRover(0, 0, "W");
+
+        marsRover.executeCommand("R");
+
+        Assertions.assertEquals("N", marsRover.getDirection());
+    }
+
+
+    @Test
+    void should_return_X0_Y0_W_when_execute_command_R() {
+        MarsRover marsRover = new MarsRover(0, 0, "S");
+
+        marsRover.executeCommand("R");
+
+        Assertions.assertEquals("W", marsRover.getDirection());
+    }
+
+    @Test
+    void should_return_X0_Y0_S_when_execute_command_R() {
+        MarsRover marsRover = new MarsRover(0, 0, "E");
+
+        marsRover.executeCommand("R");
+
+        Assertions.assertEquals("S", marsRover.getDirection());
+    }
+
+    @Test
+    void should_return_X0_Y0_E_when_execute_command_R() {
+        MarsRover marsRover = new MarsRover(0,0,"N");
+
+        marsRover.executeCommand("R");
+
+        Assertions.assertEquals("E",marsRover.getDirection());
+    }
 }
 
 
